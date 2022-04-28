fa0b7ebc31f34f7572e7278d68579d34d1131caf
gitgitWi <wiii@kakao.com>
2022-04-28 23:07:59 +0900

    Refactor (fetcher) split interface Stock, Sector from StockMap

--- src/services/fetcher/types.ts
+++ src/services/fetcher/types.ts
@@ -2,11 +2,26 @@ export type Market = "domestic" | "overseas";
 export type SubMarket = "" | "kospi" | "kosdaq" | "dow" | "nasdaq";
 export type MapPeriod = "day" | "monthly";
 
+export interface Stock {
+  name: string;
+  stockName: string;
+  stockCode: string;
+  description: string;
+  value: number;
+  priceChange: number;
+  fluctuationRate: number;
+  currentPrice: number;
+  tradingVolume: number;
+  marketCap: number;
+}
+
+export interface Sector {
+  name: string;
+  industryId: number;
+  industryName: string;
+  children: Stock[];
+}
+
 export interface StockMap {
-  children: {
-    name: string;
-    industryId: number;
-    industryName: string;
-    children: any[];
-  }[];
+  children: Sector[];
 }
